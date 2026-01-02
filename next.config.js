4f79bf8d732dab9f81498baaa25e5ab7920f5e84
Homepage Business <homepage.business.fc@gmail.com>
2026-01-02 11:14:15 +0000

    Update file next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/homepage-business-project',
+    basePath: '',
+    assetPrefix: '',
     images: {
       unoptimized: true,
     },

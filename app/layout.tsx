be36f75059414bf4552b1d1df6e18577004a3d5e
Homepage Business <homepage.business.fc@gmail.com>
2026-01-10 09:27:43 +0000

    Update 3 files
    - /public/icon_kacchau.png
    - /app/layout.tsx
    - /public/icon_kacchau.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "カッチャウ佐賀南3号店 | 車買取・販売専門店",
   description: "車を買いたい・売りたい そんな時はカッチャウにおまかせください！佐賀県内5店舗展開中。",
   icons: {
-    icon: '/favicon.ico',
+    icon: '/icon_kacchau.ico',
     apple: '/logo_kacchau.png',
   },
 };
